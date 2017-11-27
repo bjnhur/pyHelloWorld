906e83a7f200c2fa0290e232bfc94427463eb12a
Bongjun Hur <bjnhur@gmail.com>
2017-11-27 20:14:23 +0900

    init Travis-CI

---
+++ main.py
@@ -0,0 +1,13 @@
+def test_get_helloworld():
+	assert 'hello world' == get_helloworld()
+	
+def get_helloworld():
+	print('hello world')
+	return 'hello world'
+	
+def main():
+	print(get_helloworld())
+
+if __name__ == '__main__':
+	main()
+	
